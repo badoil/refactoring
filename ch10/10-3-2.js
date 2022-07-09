dc289dc1a2bc846347aa2fdf41bf06c5852630ac
oneal.oh <oneal.oh@kakaomobility.com>
2022-07-09 22:13:56 +0900

    10-3-2, 중첩조건문 보호구문으로 바꾸기2

--- ch10/10-3-2.js
+++ ch10/10-3-2.js
@@ -1,11 +1,15 @@
+// 중첩조건문 보호구문으로 바꾸기2
+
 export function adjustedCapital(instrument) {
-  let result = 0;
-  if (instrument.capital > 0) {
-    if (instrument.interestRate > 0 && instrument.duration > 0) {
-      result =
-        (instrument.income / instrument.duration) *
-        anInstrument.adjustmentFactor;
-    }
+  // 요 보호구문으로 얼리 엑싯 하기
+  if (!isTrue(instrument)) {
+    return 0;
   }
-  return result;
+  if (isTrue(instrument)) {
+    return  (instrument.income / instrument.duration) * anInstrument.adjustmentFactor;
+  }
+}
+
+function isTrue(instrument) {
+  return (instrument.capital > 0 && instrument.interestRate > 0 && instrument.duration > 0)
 }
