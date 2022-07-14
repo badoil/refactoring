a77c4dc4b54eb23254b25a6c1d40df720ec7f876
oneal.oh <oneal.oh@kakaomobility.com>
2022-07-14 17:49:54 +0900

    10-7, 제어플래그를 탈출문으로 바꾸기

--- ch10/10-7.js
+++ ch10/10-7.js
@@ -1,4 +1,12 @@
+//제어플래그를 탈출문으로 바꾸기
+// 
 for (const p of people) {
+  if (p === 'Don') {
+    sendAlert();
+    break;
+  }
+  // 아래를 위와같이 수정해야함. 불리언타입 쓰지말고 제어문법 사용
+
   if (!found) {
     if (p === 'Don') {
       sendAlert();
