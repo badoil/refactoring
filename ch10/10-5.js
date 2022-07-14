72104f821ee102d492dae0aca79435a136db0a52
oneal.oh <oneal.oh@kakaomobility.com>
2022-07-14 17:04:26 +0900

    10-5, 특이케이스 이용하기

--- ch10/10-5.js
+++ ch10/10-5.js
@@ -3,8 +3,9 @@ export class Site {
     this._customer = customer;
   }
 
+  // 아래와같이 특이케이스 클래스 UnknownCustomer를 사용
   get customer() {
-    return this._customer;
+    return this._customer === 'unknown' ? new UnknownCustomer() : new Customer(this._customer);
   }
 }
 
@@ -30,13 +31,26 @@ export class Customer {
   }
 }
 
+
+// UnknownCustomer 라는 특이케이스를 만들어서 처리
+class UnknownCustomer extends Customer {
+  get name() {
+    return 'occupant';
+  }
+}
+
 // 사용하는 부분
 export function customerName(site) {
   const aCustomer = site.customer;
-  // 더 많은 코드가 여기에 있음
-  let customerName;
-  if (aCustomer === 'unknown') customerName = 'occupant';
-  else customerName = aCustomer.name;
 
-  return customerName;
+  // 아래 코드는 다음과 같이 수정
+
+  return aCustomer.name;
+
+  // 더 많은 코드가 여기에 있음
+  // let customerName;
+  // if (aCustomer === 'unknown') customerName = 'occupant';
+  // else customerName = aCustomer.name;
+
+  // return customerName;
 }
