e7694f02f46abc9301ba651d391a4230bfd2a296
oneal.oh <oneal.oh@kakaomobility.com>
2022-07-23 16:27:28 +0900

    fix: 11-4, 객체 통째로 넘기기

--- ch11/11-4.js
+++ ch11/11-4.js
@@ -1,8 +1,11 @@
+// 객체 통째로 넘기기
+// 만약 함수가 다 넘기는 객체에 의존성이 너무 커지게 된다고 판단되면 통째로 안넘기는게 나을 수 있음
+// 케바케라 잘 판단해야함 
 export function temperatureAlerts(room, plan) {
   const alerts = [];
   const low = room.daysTempRange.low;
   const high = room.daysTempRange.high;
-  if (!plan.withinRange(low, high)) {
+  if (!plan.withinRange(room.daysTempRange)) {
     alerts.push('room temperature went outside range');
   }
 
@@ -14,9 +17,9 @@ export class HeatingPlan {
     this._temperatureRange = temperatureRange;
   }
 
-  withinRange(bottom, top) {
+  withinRange(tempRange) {
     return (
-      bottom >= this._temperatureRange.low && top <= this._temperatureRange.high
+      tempRange.bottom >= this._temperatureRange.low && tempRange.top <= this._temperatureRange.high
     );
   }
 }
