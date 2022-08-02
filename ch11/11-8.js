0aee45cfe00e4c09c68bc9cbc65e2c9a55b60291
oneal.oh <oneal.oh@kakaomobility.com>
2022-08-02 17:41:49 +0900

    fix: 11-9, 함수를 명령객체로 바꾸기

--- ch11/11-8.js
+++ ch11/11-8.js
@@ -1,5 +1,6 @@
 // 생성자를 팩토리함수로 바꾸기
 // 아래와 같이 typeCode 가 있을때 팩토리 함수로 typeCode를 미리 지정해 놓음
+// 인스턴스를 만드는 로직 자체를 캡슐화해서 외부에서 모르게하는 것
 
 export class Employee {
   constructor(name, typeCode) {
@@ -18,7 +19,8 @@ export class Employee {
     return { E: 'Engineer', M: 'Manager', S: 'Salesman' };
   }
 
-  // 아래와 같이 픽토리 함수를 이용해서 객체 생성
+  // 아래와 같이 픽토리 함수를 이용해서 객체 생성,
+  // 이처럼 인스턴스 만드는 로직이 캡슐화됨
   static engineer(name) {
     return new Employee(name, 'E');
   }
--- ch11/11-9.js
+++ ch11/11-9.js
@@ -1,21 +1,60 @@
+// 함수를 명령객체로 바꾸기
+
+// 아래와 같은 함수를 하나의 클래스로 만들면 어떨지
+// 하나의 명령을 수행하는 명령 객체
+// command object, command pattern
+
+// 명령 객체를 만들고 그 안에 do / undo 메소드 둘다 만들어 놓음
+
+// 아래와 같이 스코어 함수의 로직을 스코어러 클래스의 execute 함수 안에 구현
+// 이때 데이터들을 캡슐화 할 것인지, execute 함수 결과를 클래스안에 보관할 것인지 등을
+// 사용 방식에 따라 정해줌
+
 export function score(candidate, medicalExam, scoringGuide) {
   let result = 0;
-  let healthLevel = 0;
-  let highMedicalRiskFlag = false;
-
-  if (medicalExam.isSmoker) {
-    healthLevel += 10;
-    highMedicalRiskFlag = true;
-  }
-  let certificationGrade = 'regular';
-  if (scoringGuide.stateWithLowCertification(candidate.originState)) {
-    certificationGrade = 'low';
-    result -= 5;
-  }
-  // lots more code like this
-  result -= Math.max(healthLevel - 5, 0);
-  return result;
+    let healthLevel = 0;
+    let highMedicalRiskFlag = false;
+  
+    if (medicalExam.isSmoker) {
+      healthLevel += 10;
+      highMedicalRiskFlag = true;
+    }
+    let certificationGrade = 'regular';
+    if (scoringGuide.stateWithLowCertification(candidate.originState)) {
+      certificationGrade = 'low';
+      result -= 5;
+    }
+    // lots more code like this
+    result -= Math.max(healthLevel - 5, 0);
+    return result;
 }
+
+ class Scorer {
+   constructor(candidate, medicalExam, scoringGuide) {
+     this.candidate = candidate;
+     this.medicalExam = medicalExam;
+     this.scoringGuide = scoringGuide;
+   }
+
+   execute() {
+    let result = 0;
+    let healthLevel = 0;
+    let highMedicalRiskFlag = false;
+  
+    if (this.medicalExam.isSmoker) {
+      healthLevel += 10;
+      highMedicalRiskFlag = true;
+    }
+    let certificationGrade = 'regular';
+    if (this.scoringGuide.stateWithLowCertification(this.candidate.originState)) {
+      certificationGrade = 'low';
+      result -= 5;
+    }
+    // lots more code like this
+    result -= Math.max(healthLevel - 5, 0);
+    return result;
+   }
+ }
 
 export class ScoringGuide {
   stateWithLowCertification(state) {
